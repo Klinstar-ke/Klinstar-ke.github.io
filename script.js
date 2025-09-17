67a23bdf94664eed86587d18e6a80262071689ea
Klinstar-ke <kenedynjagih@gmail.com>
2025-09-17 12:33:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
    // e.preventDefault();
     //alert("Message sent! (Form is static for now)");
 //});
+
+// Optional success alert when submitted
+document.querySelector(".contact-form").addEventListener("submit", function () {
+  alert("Thank you! Your message has been sent.");
+});
